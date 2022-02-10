43171b1b4b4b8d9136fc901e278cb13eaa61b0e4
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2022-02-10 03:02:21 +0100

    P2B2

---
+++ Practica2Bloque2/apartado2/script.js
@@ -0,0 +1,28 @@
+function calcDescuento() {
+    let formulario = document.getElementById("formularioDescuento");
+
+    let marcaCoche = formulario.marca.value;
+    let precio = formulario.precio.value;
+
+    let descuento = 1.0;
+    switch (marcaCoche) {
+        case "ford":
+            descuento -= 0.1;
+            break;
+        case "seat":
+            descuento -= 0.08;
+            break;
+        case "citroen":
+            descuento -= 0.06;
+            break;
+        case "audi":
+            descuento -= 0.04;
+            break;
+        default:
+            descuento -= 0.0;
+            break;
+    }
+    let precioFinal = precio * descuento;
+    
+    alert("El precio final del coche es " + precioFinal + " €");
+}
