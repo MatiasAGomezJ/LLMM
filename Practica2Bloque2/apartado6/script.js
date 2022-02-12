1dd6135f7b56ad5002690b6f5fde89a3865d610e
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2022-02-12 03:34:17 +0100

    apartado6

--- Practica2Bloque2/apartado6/script.js
+++ Practica2Bloque2/apartado6/script.js
@@ -1,7 +1,8 @@
 function mostrarContenido() {
     let ps = document.getElementsByTagName("p");
 
-    for (const p in ps) {
+    for (let i = 0; i < ps.length; i++) {
+        let p = ps[i];
         p.style.display = "block";
     }
 }
