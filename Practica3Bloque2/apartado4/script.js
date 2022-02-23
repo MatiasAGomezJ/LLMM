01a067cbfbbc986d0ab4880139421b433d363d16
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2022-02-23 06:04:28 +0100

    feat: :sparkles: P2B3 a4 Añadidas checkNombre & checkTel

--- Practica3Bloque2/apartado4/script.js
+++ Practica3Bloque2/apartado4/script.js
@@ -1,27 +1,59 @@
-// * Hoy es un buen dia
-// * 22:22 22/2/22
-
 function validacio() {
     let formulario = document.getElementById("formulario");
 
-    checkMail();
-}
-
-function checkMail() {
-    let correoCaja = formulario.correo;
-    correoCaja.value = "jesuscristo@gmail.com";
-
-    if (checkIfVacio(correoCaja)) {
-        alert("El " + texto.name + " esta vacio");
-        return false;
-    }
-
-    if (!/^\w+@+\w+\.+[a-zA-Z]{2,4}$/.test(correoCaja.value)) {
-        alert("L'adreça de correu no és vàlida");
-        return false;
+    try {
+        // checkNombre();
+        // checkCorreo();
+        checkTel();
+    } catch (error) {
+        alert("Ha habido un error.\n" + error + ".");
     }
 }
 
-function checkIfVacio(texto) {
-    return texto.value == null || texto.value.length == 0 ? true : false;
+function checkNombre() {
+    let input = formulario.nombre;
+    // ! Borrar
+    input.value = "Matias";
+
+    // Esto no deberia nunca dar error ya que en el ...
+    // ... formulario tiene el atributo required. Por ...
+    // ... lo tanto jamas deberia estar vacio.
+    checkIfVacio(input);
+}
+
+function checkCorreo() {
+    let input = formulario.correo;
+    // ! Borrar
+    input.value = "gmail@gmail.com";
+
+    checkIfVacio(input);
+
+    if (!/^\w+@+\w+\.+[a-zA-Z]{2,4}$/.test(input.value)) {
+        throw "L'adreça de correu no és vàlida";
+    }
+}
+
+function checkTel() {
+    let input = formulario.telefono;
+    // ! Borrar
+    input.value = "971456289";
+
+    // Esto no deberia nunca dar error ya que en el ...
+    // ... formulario tiene el atributo required. Por ...
+    // ... lo tanto jamas deberia estar vacio.
+    checkIfVacio(input);
+
+    if (isNaN(Number(input.value))) {
+        throw "El numero de teléfono solo debe tener caracteres numéricos";
+    }
+
+    if (input.value.length != 9) {
+        throw "El numero de " + input.name + " tiene que tener exactamente 9 digitos";
+    }
+}
+
+function checkIfVacio(cajaTexto) {
+    if (cajaTexto.value == null || cajaTexto.value.length == 0) {
+        throw "El " + cajaTexto.name + " esta vacio";
+    }
 }
