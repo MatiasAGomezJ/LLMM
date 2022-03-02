ecd2d2b8b69842d4cecb7707080666a675c16c2a
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2022-03-03 00:17:30 +0100

    refactor: :recycle: Las variables dentro de las funcinoes de checkeo ahora guardan el valor en vez de la caja

--- Practica3Bloque2/apartado4/script.js
+++ Practica3Bloque2/apartado4/script.js
@@ -6,61 +6,59 @@ function validacio() {
         checkCorreo();
         checkTelefono();
         checkContraseña();
+
+        texto = "Informacion" + "\nNom: " + formulario.nombre.value + "\nCorreo electronico: " + formulario.correo.value;
+        if (formulario.telefono.value != "") {
+            texto += "\nTelefono: " + formulario.telefono.value;
+        }
+        alert(texto);
     } catch (error) {
         alert("Ha habido un error.\n" + error + ".");
     }
-
-    texto = "Informacion" + "\nNom: " + formulario.nombre.value + "\nCorreo electronico: " + formulario.correo.value;
-
-    if (formulario.telefono.value != "") {
-        texto += "\nTelefono: " + formulario.telefono.value;
-    }
-
-    alert(texto);
 }
 
 function checkCorreo() {
-    let input = formulario.correo;
+    let valorCorreo = formulario.correo.value;
 
     let regEx = /^\w+@+\w+\.+[a-zA-Z]{2,4}$/;
 
-    if (!regEx.test(input.value)) {
+    if (!regEx.test(valorCorreo)) {
         throw "La direccion del correo no és vàlida";
     }
 }
 
 function checkTelefono() {
-    let input = formulario.telefono;
+    let valorTel = formulario.telefono.value;
 
     // Si el input está vacio para la funcion
-    if (input.value == "") return;
+    if (valorTel == "") return;
 
-    if (isNaN(Number(input.value))) {
+    if (isNaN(Number(valorTel))) {
         throw "El numero de teléfono solo debe tener caracteres numéricos";
     }
 
-    if (input.value.length != 9) {
-        throw "El numero de " + input.name + " tiene que tener exactamente 9 digitos";
+    if (valorTel.length != 9) {
+        throw "El numero de telefono tiene que tener exactamente 9 digitos";
     }
 }
 
 function checkContraseña() {
-    let input = formulario.contraseña;
+    let valorContra = formulario.contraseña.value;
 
     // Si el input está vacio para la funcion
-    if (input) return;
+    if (valorContra == "") return;
 
-    if (input.value.length < 6) {
+    if (valorContra.length < 6) {
         throw "La contraseña tiene que tener mínimo 6 carácteres";
     }
 
-    if (input.value.length > 10) {
+    if (valorContra.length > 10) {
         throw "La contraseña tiene que tener máximo 10 carácteres";
     }
 
     let regEx = /^\w+[0-9]{2}$/;
 
-    if (!regEx.test(input.value)) {
+    if (!regEx.test(valorContra)) {
         throw "La contraseña no es valida";
     }
 }
