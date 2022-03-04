28c05ab9d58645d3e6aa2542e21fb1cbd01af2dc
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2022-03-04 20:48:18 +0100

    feat: PDF acabao

--- Practica3Bloque2/apartado1/script.js
+++ Practica3Bloque2/apartado1/script.js
@@ -6,3 +6,27 @@ try {
 } catch (error) {
     alert("Ha habido un error.\n" + error);
 }
+
+function comprovar() {
+    var num = document.getElementById("x").value;
+    try {
+        if (num == "") {
+            throw "No s'ha introduït cap número";
+        }
+        if (isNaN(num)) {
+            throw "La dada introduïda no és un número";
+        }
+        num = Number(num);
+        if (num > 10) {
+            throw "el número introduït és massa gran";
+        }
+        if (num < 5) {
+            throw "el número introduït és massa petit";
+        }
+    } catch (err) {
+        alert("Error: " + err + ".");
+    } finally {
+        document.getElementById("x").value = " ";
+    }
+}
+
